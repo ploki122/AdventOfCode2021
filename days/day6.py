46afd2ebf7dd7f1ee6620bf2d48cc0c5fb032264
Ploki122 <blanchjulien@hotmail.com>
2021-12-06 14:17:56 -0500

    Day 6

---
+++ days/day6.py
@@ -0,0 +1,50 @@
+from utils.aoc_utils import AOCDay, day
+
+@day(6)
+class Day6(AOCDay):
+
+    def common(self):
+        print("\n\n== Common ==")        
+        return 0
+
+    def part1(self):
+        print("\n\n== Part 1 ==")
+        
+        parsedData = [0]*10
+        for fish in self.inputData[0].split(","):
+            parsedData[int(fish)] += 1
+
+        print("Day 0 : " + str(parsedData))
+        nbDay = 1
+        while nbDay <= 80:
+            parsedData[7] += parsedData[0]
+            parsedData[9] += parsedData[0]
+            for i in range(0, len(parsedData)-1):
+                parsedData[i] = parsedData[i+1]
+            parsedData[9] = 0
+                
+            print("Day " + str(nbDay) + " : " + str(parsedData))
+            nbDay += 1
+
+        return sum(parsedData)
+    
+    def part2(self):
+        print("\n\n== Part 2 ==")
+        
+        parsedData = [0]*10
+        for fish in self.inputData[0].split(","):
+            parsedData[int(fish)] += 1
+
+        print("Day 0 : " + str(parsedData))
+        nbDay = 1
+        while nbDay <= 256:
+            parsedData[7] += parsedData[0]
+            parsedData[9] += parsedData[0]
+            for i in range(0, len(parsedData)-1):
+                parsedData[i] = parsedData[i+1]
+            parsedData[9] = 0
+                
+            print("Day " + str(nbDay) + " : " + str(parsedData))
+            nbDay += 1
+
+        return sum(parsedData)
