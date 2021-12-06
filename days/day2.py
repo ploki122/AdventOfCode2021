d65cbdaea05835781ccb6a697a13d3826bc522e3
Ploki122 <blanchjulien@hotmail.com>
2021-12-06 09:57:56 -0500

    Day2

--- days/day2.py
+++ days/day2.py
@@ -2,12 +2,44 @@ from utils.aoc_utils import AOCDay, day
 
 @day(2)
 class Day2(AOCDay):
+    parsedData = []
+
     def common(self):
-        print(self.inputData)
+        for line in self.inputData:
+            self.parsedData.append(line.split(" "))
+
         return 0
 
     def part1(self):
-        return 0
+        coords = [0, 0]
+        for actions in self.parsedData:
+            if (actions[0] == "forward"):
+                coords[0] += int(actions[1])
+            elif (actions[0] == "back"):
+                coords[0] -= int(actions[1])
+            elif (actions[0] == "up"):
+                coords[1] -= int(actions[1])
+            elif (actions[0] == "down"):
+                coords[1] += int(actions[1])
+            else:
+                print("command is sus : " + actions[0])
+
+        return coords[0] * coords[1]
     
     def part2(self):
-        return 0
+        coords = [0, 0, 0] # X, Y, Aim
+        for actions in self.parsedData:
+            if (actions[0] == "forward"):
+                coords[0] += int(actions[1])
+                coords[1] += int(actions[1]) * coords[2]
+            elif (actions[0] == "back"):
+                coords[0] -= int(actions[1])
+                coords[1] -= int(actions[1]) * coords[2]
+            elif (actions[0] == "up"):
+                coords[2] -= int(actions[1])
+            elif (actions[0] == "down"):
+                coords[2] += int(actions[1])
+            else:
+                print("command is sus : " + actions[0])
+
+        return coords[0] * coords[1]
